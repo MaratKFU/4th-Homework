d8839bc70bea19b0201522ed7cfa2e184e59fb6f
MaratKFU <maraturai228@gmail.com>
2024-11-21 05:48:08 +0300

    Add files via upload

---
+++ Homework №4/Program.cs
@@ -0,0 +1,310 @@
+﻿using System;
+using System.Collections.Generic;
+namespace Homework__4
+{
+
+    public enum GrouchinessLvl
+    {
+        s, a, b, c, d, e, f
+    }
+    public struct Ded
+    {
+        public string name;
+        public string[] swearingPhrases;
+        public byte sinyaki0;
+        public GrouchinessLvl g;
+        public byte SinyakiOtBabki(Ded ded ,params string[] phrases)
+        {
+            byte sinyaki = ded.sinyaki0;
+            string[] phrases0 = ded.swearingPhrases;
+            foreach (string s0 in phrases0) 
+            {
+                foreach (string s in phrases)
+                {
+                    if (s0 == s)
+                    {
+                        sinyaki++;
+                    }
+                }
+            }
+            return sinyaki;
+        }
+    }
+    internal class Program
+    {
+        static double MasArythms(ref double multiply, out double masAverage, params double[] numbers)
+        {
+            double sum = 0;
+            
+            foreach (var number in numbers)
+            {
+                multiply *= number;
+            }
+
+            foreach (var number in numbers)
+            {
+                sum += number;
+            }
+
+            masAverage = sum/numbers.Length;
+
+            return sum;
+        }
+        static void Main(string[] args)
+        {
+            //1
+            
+            Console.WriteLine("1 Упражнение");
+
+            int[] arr = new int[20];
+            Random random = new Random();
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = random.Next(-100000, 100000);
+                Console.WriteLine(arr[i]);
+            }
+
+            Console.WriteLine("Введите числа, которые хотите поменять местами:");
+
+            int.TryParse(Console.ReadLine(), out var a);
+            int.TryParse(Console.ReadLine(), out var b);
+            
+            bool flag1 = false, flag2 = false;
+            
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (a == arr[i])
+                {
+                    a = i;
+                    flag1 = true;
+                    break;
+                }
+            }
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (b == arr[i])
+                {
+                    b = i;
+                    flag2 = true;
+                    break;
+                }
+            }
+            if (flag1 && flag2)
+            {
+                int c = arr[a];
+                arr[a] = arr[b];
+                arr[b] = c;
+                
+                Console.WriteLine("Вот массив с поменявшимися местами указанными элементами ");
+                
+                for (int i = 0; i < arr.Length; i++)
+                {
+                    Console.WriteLine(arr[i]);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Указанного(ых) элемента(ов) нет в данном массиве");
+            }
+
+            Console.WriteLine();
+
+            //2
+            Console.WriteLine("2 Упражнение");
+            double multiply = 1;
+            double masAverage = 0;
+            
+            List<double> mas = new List<double>();
+            
+            Console.Write("Введите количество элементов массива:");
+            uint.TryParse(Console.ReadLine(), out var len);
+
+            Console.WriteLine("Введите элементы массива построчно:");
+            for (int i = 0; i < len; i++) 
+            {
+                double.TryParse(Console.ReadLine(), out var m);
+                mas.Add(m);
+            }
+            
+            double[] mas1 = mas.ToArray();
+
+            Console.WriteLine($"Сумма элементов массива равна: {MasArythms(ref multiply, out masAverage, mas1)}");
+            Console.WriteLine($"Произведение элементов массива равно:{multiply}");
+            Console.WriteLine($"Среднее арифметическое элементов массива равно:{masAverage}");
+
+            Console.WriteLine();
+
+            //3
+            Console.WriteLine("3 Упражнение");
+            Console.WriteLine("Введите целое число от 0 до 9");
+
+            int digit = Convert.ToInt32(Console.ReadLine());
+            switch (digit)
+            {
+                case 0:
+                    Console.WriteLine("###");
+                    Console.WriteLine("# #");
+                    Console.WriteLine("# #");
+                    Console.WriteLine("# #");
+                    Console.WriteLine("###");
+                    break;
+                case 1:
+                    Console.WriteLine("  #");
+                    Console.WriteLine(" ##");
+                    Console.WriteLine("# #");
+                    Console.WriteLine("  #");
+                    Console.WriteLine("  #");
+                    break;
+                case 2:
+                    Console.WriteLine("###");
+                    Console.WriteLine("  #");
+                    Console.WriteLine("###");
+                    Console.WriteLine("#  ");
+                    Console.WriteLine("###");
+                    break;
+                case 3:
+                    Console.WriteLine("###");
+                    Console.WriteLine("  #");
+                    Console.WriteLine("###");
+                    Console.WriteLine("  #");
+                    Console.WriteLine("###");
+                    break;
+                case 4:
+                    Console.WriteLine("# #");
+                    Console.WriteLine("# #");
+                    Console.WriteLine("###");
+                    Console.WriteLine("  #");
+                    Console.WriteLine("  #");
+                    break;
+                case 5:
+                    Console.WriteLine("###");
+                    Console.WriteLine("#  ");
+                    Console.WriteLine("###");
+                    Console.WriteLine("  #");
+                    Console.WriteLine("###");
+                    break;
+                case 6:
+                    Console.WriteLine("###");
+                    Console.WriteLine("#  ");
+                    Console.WriteLine("###");
+                    Console.WriteLine("# #");
+                    Console.WriteLine("###");
+                    break;
+                case 7:
+                    Console.WriteLine("###");
+                    Console.WriteLine("  #");
+                    Console.WriteLine(" # ");
+                    Console.WriteLine("#  ");
+                    Console.WriteLine("# ");
+                    break;
+                case 8:
+                    Console.WriteLine("###");
+                    Console.WriteLine("# #");
+                    Console.WriteLine("###");
+                    Console.WriteLine("# #");
+                    Console.WriteLine("###");
+                    break;
+                case 9:
+                    Console.WriteLine("###");
+                    Console.WriteLine("# #");
+                    Console.WriteLine("###");
+                    Console.WriteLine("  #");
+                    Console.WriteLine("###");
+                    break;
+                default:
+                    Console.BackgroundColor = ConsoleColor.Red;
+
+                    Console.WriteLine("### # # # #   # ##### #  #   #   #");
+                    Console.WriteLine("# # # # # #  ## #     # #   # #  #");
+                    Console.WriteLine("# # # # # # # # ##### ##   #   # #");
+                    Console.WriteLine("# # # # # ##  # #   # # #  ##### ");
+                    Console.Write    ("### ##### #   # ##### #  # #   # #");
+                    
+                    System.Threading.Thread.Sleep(3000);
+                    Console.BackgroundColor = ConsoleColor.Black;
+                    break;
+            }
+            Console.WriteLine();
+            Console.WriteLine("Введите 'закрыть' или 'exit' если хотите закрыть консоль");
+            string exit = Console.ReadLine();
+            if (exit == "exit" || exit == "закрыть")
+            {
+                Environment.Exit(0);
+            }
+            
+            Console.WriteLine();
+
+            //4
+            Console.WriteLine("4 Упражнение");
+
+            Console.WriteLine();
+
+            string[] babkasWords = { "Дураки", "Мрази", "Ироды", "Погань", "Остолопы" };
+            
+            Ded ded1 = new Ded();
+            ded1.name = "Иван";
+            ded1.g = GrouchinessLvl.e;
+            string[] swearingPh1 = {"Дураки", "Гады", "Негодяи"};
+            ded1.swearingPhrases = swearingPh1;
+            ded1.sinyaki0 = 1;
+            
+            Console.WriteLine($"Дед {ded1.name}");
+            Console.WriteLine($"Количество синяков до бабки: {ded1.sinyaki0}");
+            Console.WriteLine($"Количество синяков после бабки:{ded1.SinyakiOtBabki(ded1, babkasWords)}");
+
+            Console.WriteLine();
+
+            Ded ded2 = new Ded();
+            ded2.name = "Степан";
+            ded2.g = GrouchinessLvl.b;
+            string[] swearingPh2 = { "Дураки", "Гады", "Ироды", "Лиходеи", "Скотины" };
+            ded2.swearingPhrases = swearingPh2;
+            ded2.sinyaki0 = 2;
+
+            Console.WriteLine($"Дед {ded2.name}");
+            Console.WriteLine($"Количество синяков до бабки: {ded2.sinyaki0}");
+            Console.WriteLine($"Количество синяков после бабки:{ded2.SinyakiOtBabki(ded2, babkasWords)}");
+
+            Console.WriteLine();
+
+            Ded ded3 = new Ded();
+            ded3.name = "Виталий";
+            ded3.g = GrouchinessLvl.c;
+            string[] swearingPh3 = {"Дармоеды", "Гады", "Дураки", "Погань", "Негодяи" };
+            ded3.swearingPhrases = swearingPh3;
+
+            Console.WriteLine($"Дед {ded3.name}");
+            Console.WriteLine($"Количество синяков до бабки: {ded3.sinyaki0}");
+            Console.WriteLine($"Количество синяков после бабки:{ded3.SinyakiOtBabki(ded3, babkasWords)}");
+
+            Console.WriteLine();
+
+            Ded ded4 = new Ded();
+            ded4.name = "Анатолий";
+            ded4.g = GrouchinessLvl.d;
+            string[] swearingPh4 = { "Дураки", "Ироды", "Лиходеи", "Остолопы" };
+            ded4.swearingPhrases = swearingPh4;
+            ded4.sinyaki0 = 2;
+
+            Console.WriteLine($"Дед {ded4.name}");
+            Console.WriteLine($"Количество синяков до бабки: {ded4.sinyaki0}");
+            Console.WriteLine($"Количество синяков после бабки:{ded4.SinyakiOtBabki(ded4, babkasWords)}");
+
+            Console.WriteLine();
+
+            Ded ded5 = new Ded();
+            ded5.name = "Митрофан";
+            ded5.g = GrouchinessLvl.s;
+            string[] swearingPh5 = { "Дураки", "Ироды", "Лиходеи", "Остолопы", "Погань", "Дармоеды", "Гады", "Негодяи", "Скотины", "Мрази"};
+            ded5.swearingPhrases = swearingPh5;
+            ded5.sinyaki0 = 15;
+
+            Console.WriteLine($"Дед {ded5.name}");
+            Console.WriteLine($"Количество синяков до бабки: {ded5.sinyaki0}");
+            Console.WriteLine($"Количество синяков после бабки:{ded5.SinyakiOtBabki(ded5, babkasWords)}");
+
+        }
+    }
+}
---
+++ Tumakov/Program.cs
@@ -0,0 +1,211 @@
+﻿using System;
+
+namespace Tumakov
+{
+    internal class Program
+    {
+        static long Max1(long a, long b)
+        {
+            if (a > b) 
+            {
+                return a;
+            }
+            return b;
+        }
+
+        
+        static void SwapValues(ref string a, ref string b)
+        {
+            string c = a; a = b; b = c;
+        }
+        static ulong RecFactorial(ulong n)
+        {
+            if (n == 0)
+            {
+                return 1;
+            }
+            return RecFactorial(n-1) * n;
+        }
+
+        static bool TryFactorial(ulong n)
+        {
+            ulong res = 1;
+
+            try
+            {
+                checked
+                {
+                    for (ulong i = 1; i <= n; i++)
+                    {
+                        res *= i;
+                    }
+                }
+                return true;
+            }
+            catch (OverflowException) 
+            {
+                return false;
+            }
+        }
+
+        static ulong GCD(ulong a, ulong b)
+        {
+            if (a == 0 || b == 0)
+            {
+                return a + b;
+            }
+            if (a > b)
+            {
+                return GCD(a % b, b);
+
+            }
+            return GCD(a, b % a);
+        }
+
+        static ulong GCD(ulong a, ulong b, ulong c)
+        {
+            return GCD(a, GCD(b, c));
+        }
+
+
+        static ulong Fibonacci(byte n)
+        {
+            ulong a = 1;
+            ulong b = 1;
+            for (byte i = 2;i < n; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    a += b;
+                }
+                else
+                {
+                    b += a;
+                }
+            }
+            return Math.Max(a, b);
+        }
+        
+        static void Main(string[] args)
+        {
+            //1 Упражнение
+            
+            Console.WriteLine("Упражнение 5.1");
+
+            Console.WriteLine("Введите два целых числа:");
+            
+            long.TryParse(Console.ReadLine(), out var a);
+            long.TryParse(Console.ReadLine(), out var b);
+            
+            Console.WriteLine($"Наибольшее из них равняется {Max1(a, b)}");
+            
+            Console.WriteLine();
+            
+            
+            
+            //2 Упражнение
+            
+            Console.WriteLine("Упражнение 5.2");
+            
+            Console.WriteLine("Введите два значения:");
+            
+            string param1 = Console.ReadLine();
+            string param2 = Console.ReadLine();
+            
+            SwapValues(ref param1, ref param2);
+            Console.WriteLine($"Вот ваши значения, но поменявшиеся местами:{param1} {param2}");
+            
+            Console.WriteLine();
+            
+            
+            
+            //3 Упражнение
+
+            Console.WriteLine("Упражнение 5.3");
+            
+            Console.Write("Введите значение числа n для проверки на переполнение значений типа ulong для числа n!:");
+            
+            ulong.TryParse(Console.ReadLine(), out var n1);
+            
+            if (TryFactorial(n1))
+            {
+                Console.WriteLine($"Вычисление числа {n1}! не вызвало переполнения значений типа ulong");
+            }
+
+            else
+            {
+                Console.WriteLine($"Вычисление числа {n1}! вызвало переполнение значений типа ulong");
+            }
+            
+            Console.WriteLine();
+            
+            
+            
+            //4 Упражнение
+            
+            Console.WriteLine("Упражнение 5.4");
+            
+            Console.Write("Введите значение числа n для вычисления n!:");
+            
+            ulong.TryParse(Console.ReadLine(), out var n2);
+            Console.WriteLine($"{n2}! = {RecFactorial(n2)}");
+
+            Console.WriteLine();
+            
+
+            
+            //1 ДЗ
+            
+            Console.WriteLine("Домашнее задание 5.1");
+            
+            Console.WriteLine("Эта программа находит НОД двух или трёх чисел");
+            Console.WriteLine("Введите количество чисел, для которых хотите вычислить НОД:");
+            
+            byte.TryParse(Console.ReadLine(), out var n);
+            
+            switch (n){
+                case 2:
+                    Console.WriteLine("Введите эти два числа построчно:");
+
+                    ulong.TryParse(Console.ReadLine(), out var a1);
+                    ulong.TryParse(Console.ReadLine(), out var b1);
+
+                    Console.WriteLine(GCD(a1, b1));
+                    break;
+                
+                case 3:
+                    Console.WriteLine("Введите эти три числа построчно:");
+                    
+                    ulong.TryParse(Console.ReadLine(), out a1);
+                    ulong.TryParse(Console.ReadLine(), out b1);
+                    ulong.TryParse(Console.ReadLine(), out var c1);
+                    
+                    Console.WriteLine(GCD(a1, b1, c1));
+                    break;
+            
+                default:
+                    Console.WriteLine($"Данная программа не умеет вычислять НОД для {n} чисел");
+                    break;
+
+            }
+            
+            
+            
+            //2 ДЗ
+            
+            Console.WriteLine("Домашнее задание 5.2");
+            Console.WriteLine("Введите число n для вычисления n-ного числа Фибоначчи");
+            
+            byte.TryParse(Console.ReadLine(), out var i);
+            
+            if (i == 0)
+            {
+                Console.WriteLine("Не существует числа с таким порядковым номером");
+            }
+            else 
+            {
+                Console.WriteLine($"Число Фибоначчи с порядковым номером {i} равно {Fibonacci(i)}");
+            }
+        }
+    }
+}
